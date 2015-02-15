655e278f8ed4e072d3ab07cefee1ac92b2bd816e
KentLeong <EricKentLeongChu@gmail.com>
2015-02-14 22:42:05 -0500

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -146,25 +146,23 @@ static const int GRID_COLUMNS = 10;
     return isIndexValid;
 }
 
-- (void)updateCreatures{
-    int numAlive = 0;
-    for (int i=0; i < [_gridArray count]; i++) {
-        for (int j=0; j< [_gridArray[i] count]; j++) {
-            
+- (void)updateCreatures {
+    _totalAlive = 0;
+    
+    for (int i = 0; i < [_gridArray count]; i++) {
+        for (int j = 0; j < [_gridArray[i] count]; j++) {
             Creature *currentCreature = _gridArray[i][j];
-            
             if (currentCreature.livingNeighbors == 3) {
                 currentCreature.isAlive = YES;
-            } else if (currentCreature.livingNeighbors <= 1 || currentCreature.livingNeighbors >=4){
+            } else if ( (currentCreature.livingNeighbors <= 1) || (currentCreature.livingNeighbors >= 4)) {
                 currentCreature.isAlive = NO;
             }
             
             if (currentCreature.isAlive) {
-                numAlive ++;
+                _totalAlive++;
             }
         }
     }
-    _totalAlive = numAlive;
 }
 
 @end
